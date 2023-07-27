b26d3ff37ddbea1823c423c590cdab7a4d95dd02
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-07-27 11:01:48 +0300

    Add variations to product

---
+++ db/migrate/20230727080046_create_variations.rb
@@ -0,0 +1,11 @@
+class CreateVariations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :variations do |t|
+      t.string :name
+      t.string :value
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
