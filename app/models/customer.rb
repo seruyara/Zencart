b736e5e47e012b185250334d47fe04fb08433e4b
ivymarubu <ivymarubu@gmail.com>
2023-07-27 15:06:16 +0300

    IvyBranch

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
     has_one :cart 
     has_many :orders
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,3 +1,7 @@
 class Seller < ApplicationRecord
     has_one :shop
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :shop_name, presence: true, uniqueness: true
 end
