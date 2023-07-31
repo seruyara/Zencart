c630f57ca5dcb2e8ff7c067d1c264ca68d94ed05
ivymarubu <ivymarubu@gmail.com>
2023-07-31 18:06:35 +0300

    routesrb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:show, :index, :update, :destroy]
   resources :products, only: [:show, :index, :update, :destroy]
   resources :shops, only: [:show, :index, :update, :destroy]
-  resources :customers, only: [:show, :index, :update, :destroy]
-  resources :sellers, only: [:show, :index, :update, :destroy]
-  
+  resources :customers, only: [:show, :index, :create, :update, :destroy]
+  #resources :sellers, only: [:show, :index,  :update, :destroy]
+  post '/auth/login', to: 'authentication#login'
+  post '/customers', to: 'customers#create' 
 end
