a54adef9be119b5f393b68c0b044f1275ce97a48
ivymarubu <ivymarubu@gmail.com>
2023-07-31 18:03:42 +0300

    customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,59 +1,48 @@
 class CustomersController < ApplicationController
 
-  skip_before_action :authenticateuser, only: [:create]
-  before_action :find_user, only: [:show, :update, :destroy]
+  skip_before_action :authenticate_customer, only: [:create]
+  before_action :find_customer, only: [:show, :update, :destroy]
 
   # GET /customers
   def index
-    customer = Customer.all
-    render json: customer
+    @customers = Customer.all
+    render json: @customers
   end
 
   # GET /customers/1
   def show
-    customer = set_customer
-    render json: customer
+    render json: @customer
   end
 
   # POST /customers
   def create
-    customer = Customer.create!(customer_params)
-    if customer
-      render json: customer, status: :created
-    else 
-      render json: {error: 'customer not found'}, status: :unprocessable_entity
+    @customer = Customer.create(customer_params)
+    if @customer.save
+      render json: @customer
+    else
+      render json: { errors: @customer.errors.full_messages }, status: 422
     end
   end
 
   # PATCH/PUT /customers/1
   def update
-    customer = set_customer
-    if customer.update!(customer_params)
-      render json: customer
-    else 
-      render json: {error: 'customer not found'}, status: :unprocessable_entity
+    unless @customer.update(customer_params)
+      render json: { errors: @customer.errors.full_messages }, status: 422
     end
   end
 
   # DELETE /customers/1
   def destroy
-    customer = set_customer
-    if customer
-      customer.destroy
-      head :no_content
-    else 
-      render json: {error: 'customer not found'}, status: :unprocessable_entity
-    end
+    @customer.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_customer
-      customer = Customer.find_by(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def customer_params
-      params.permit(:name, :email, :password)
+      params.require(:customer).permit(:name, :email, :password)
+    end
+    # Only allow a list of trusted parameters through.
+    def find_customer
+      @customer = Customer.find(params[:id])
     end
 end
