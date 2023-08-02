596bc6770363a4ffc7f2b63de23d541074fa5c24
ivymarubu <ivymarubu@gmail.com>
2023-08-02 20:47:35 +0300

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,21 @@ Rails.application.routes.draw do
   resources :shops, only: [:show, :index, :update, :destroy]
   resources :customers, only: [:show, :index, :create, :update, :destroy]
   resources :sellers, only: [:show, :index, :create,  :update, :destroy]
-  post '/auth/login', to: 'authentication#login'
+  post 'login', to: 'sessions#create'
+  post 'signup', to: 'customers#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/customers', to: 'customers#index'
+  delete '/customers', to: 'customers#destroy'
+
+
   post '/customers', to: 'customers#create'
   post '/sellers', to: 'sellers#create'
+  delete '/sellers', to: 'sellers#destroy'
+
   resources :categories, only: [:show, :index, :update, :destroy]
   resources :variations, only: [:show, :index, :update, :destroy]
 
   # Additional routes for search and filter actions
   get '/products/search', to: 'products#search'
   get '/products/filter', to: 'products#filter'
-
 end
