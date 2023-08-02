2a0a6103bf6756d9fb9c2682d47024d522e31483
ivymarubu <ivymarubu@gmail.com>
2023-08-02 20:45:18 +0300

    customermodel

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,25 @@
+require 'jwt'
 class Customer < ApplicationRecord
     has_one :cart 
     has_many :orders
     has_secure_password
     validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
+    
+    before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    # debugger
+    JWT.encode self.attributes, nil, 'none'
+
+    # self.token = SecureRandom.hex(32)
+  end
+
+  def reset_authentication_token!
+    self.token = nil
+  end
+
+  def valid_authentication_token?(token)
+    self.token == token
+  end
 end
