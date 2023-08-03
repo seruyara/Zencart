705652fd8dd36190e9f54787db689e77fd326412
Sheila Seruya <Sheilaseruya@gmail.com>
2023-08-03 09:35:46 +0300

    cokies,ans cors implimentation

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,5 @@ module Zencart
     # Use SameSite=Strict for all cookies to help protect against CSRF
    # https://owasp.org/www-community/SameSite
    config.action_dispatch.cookies_same_site_protection = :strict
-
   end
 end
