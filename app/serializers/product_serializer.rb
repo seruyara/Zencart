f9beeef1ceeb5ba612baaefd7511b03d204a9672
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-08-07 10:12:39 +0300

    Add variation to product

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :image, :quantity, :category
+  attributes :id, :name, :description, :price, :image, :quantity, :variations, :tags, :category
   belongs_to :shop
   has_many :cart_items
 end
