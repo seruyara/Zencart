280f23a2b84c1d93201ca4b17678ff763df48be8
ivymarubu <ivymarubu@gmail.com>
2023-08-08 05:17:53 +0300

    sellermodel

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -6,19 +6,4 @@ class Seller < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     
-    before_create :generate_authentication_token!
-
-  def generate_authentication_token!
-    # debugger
-    JWT.encode self.attributes, nil, 'none'
-
-  end
-
-  def reset_authentication_token!
-    self.token = nil
-  end
-
-  def valid_authentication_token?(token)
-    self.token == token
-  end
 end
